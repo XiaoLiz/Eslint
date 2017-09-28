6351c9ae5eb22a0450305d6bbe54c97c04c3056e
liyi <liyi@liyideMac-mini.local>
2017-09-28 18:48:31 +0800

    git add gitignore files

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# eslint
+# eslint config
+
+
+
---
+++ index.js
@@ -0,0 +1,15 @@
+function add (a, b) {return a + b;
+
+}
+add();
+
+function test() {
+
+
+}
+test();
+
+function function_name() {
+	
+}
+function_name();
